bcd606510b16a31ce1d1fc8f79480b2d36f83016
tacohuang <tacohuang@tencent.com>
2022-07-18 01:25:44 +0800

    feat: slots & group

--- vite-project/src/components/container.tsx
+++ vite-project/src/components/container.tsx
@@ -1,7 +1,7 @@
-import { useEffect } from 'react';
+import { useEffect, useMemo } from 'react';
 import { useCanvas } from './canvas';
 import { useWebGpu } from './device';
-import { usePipeline } from './pipeline';
+import { usePipeline, useGroup } from './pipeline';
 
 /**
  *  录制命令
@@ -9,13 +9,16 @@ import { usePipeline } from './pipeline';
  * @param context
  * @param pipeline
  */
-const useCommandsEffect = (
-  device: GPUDevice | null | undefined,
-  context: GPUCanvasContext | undefined,
-  pipeline: GPURenderPipeline | undefined,
-) => {
+const useCommandsEffect = (options: {
+  device: GPUDevice | null | undefined;
+  context: GPUCanvasContext | undefined;
+  pipeline: GPURenderPipeline | undefined;
+  vertexBuffer: GPUBuffer | undefined;
+  group: GPUBindGroup | undefined | null;
+}) => {
   useEffect(() => {
-    if (device && context && pipeline) {
+    const { device, context, pipeline, vertexBuffer, group } = options;
+    if (device && context && pipeline && vertexBuffer && group) {
       const encoder = device.createCommandEncoder();
       const pass = encoder.beginRenderPass({
         colorAttachments: [
@@ -28,12 +31,14 @@ const useCommandsEffect = (
         ],
       });
       pass.setPipeline(pipeline);
+      pass.setVertexBuffer(0, vertexBuffer);
+      pass.setBindGroup(0, group);
       pass.draw(3); // 使用多少个线程运行vertexshader，这里与数量一致（会并行运行3次）
       pass.end();
       const commandBuffer = encoder.finish();
       device.queue.submit([commandBuffer]);
     }
-  }, [device, context, pipeline]);
+  }, [options]);
 };
 
 /**
@@ -43,8 +48,19 @@ const useCommandsEffect = (
 const Container = () => {
   const { device } = useWebGpu();
   const { dom: canvas, context } = useCanvas(device);
-  const pipeline = usePipeline(device);
-  useCommandsEffect(device, context, pipeline);
+  const { pipeline, vertexBuffer, colorBuffer } = usePipeline(device);
+  const groupConf = useMemo(() => {
+    if (colorBuffer && device && pipeline) {
+      return {
+        groupDataEntries: [{ binding: 0, resource:{buffer: colorBuffer} }],
+        device,
+        pipeline,
+      };
+    }
+    return undefined;
+  }, [device, pipeline, colorBuffer]);
+  const group = useGroup(groupConf);
+  useCommandsEffect({ device, context, pipeline, vertexBuffer, group });
   return canvas;
 };
 
--- vite-project/src/components/pipeline.tsx
+++ vite-project/src/components/pipeline.tsx
@@ -1,7 +1,42 @@
 import vertex from '../wgsl/triangle.vert.wgsl?raw';
 import frag from '../wgsl/red.frag.wgsl?raw';
 import { useDefaultColorFormat } from './canvas';
-import { useEffect, useState } from 'react';
+import { useEffect, useMemo, useState } from 'react';
+/**
+ *  buffer
+ * UNIFORM : 最大64kb
+ * STORAGE : 最大2gb
+ * @param initNumberArr
+ * @param device
+ * @returns
+ */
+const useBuffers = (config: {
+  init: number[];
+  position?: number;
+  device?: GPUDevice | null;
+  usage?: number;
+}) => {
+  const { init, device, position, usage = GPUBufferUsage.UNIFORM } = config;
+  const [buf, setBuf] = useState<GPUBuffer>();
+  const data = new Float32Array(init);
+  useEffect(() => {
+    if (device) {
+      const buffer = device.createBuffer({
+        size: data.byteLength,
+        usage: usage,
+      });
+      setBuf(buffer);
+      device.queue.writeBuffer(buffer, position || 0, data);
+    }
+  }, [device]);
+  return buf;
+};
+/**
+ *  数据的存储的大小，bytes
+ */
+const useDefaultStride = () => {
+  return 3 * 4;
+};
 
 /**
  *  初始化管线
@@ -9,7 +44,23 @@ import { useEffect, useState } from 'react';
  */
 export const usePipeline = (device?: GPUDevice | null) => {
   const format = useDefaultColorFormat();
+  const defaultStride = useDefaultStride();
   const [pipeline, setPipeline] = useState<GPURenderPipeline>();
+
+  // 顶点slot的buffer
+  const vertexBuffer = useBuffers({
+    init: [0.0, 0.5, 0, -0.5, -0.5, 0, 0.5, -0.5, 0],
+    position: 0,
+    device,
+    usage: GPUBufferUsage.COPY_DST | GPUBufferUsage.VERTEX,
+  });
+  // 颜色的buffer，外面binding用
+  const colorBuffer = useBuffers({
+    init: [1, 0, 0, 1],
+    position: 0,
+    device,
+    usage: GPUBufferUsage.COPY_DST | GPUBufferUsage.UNIFORM,
+  });
   useEffect(() => {
     if (device) {
       const vertexShader = device.createShaderModule({
@@ -23,6 +74,14 @@ export const usePipeline = (device?: GPUDevice | null) => {
           vertex: {
             entryPoint: 'main',
             module: vertexShader,
+            buffers: [
+              {
+                arrayStride: defaultStride,
+                attributes: [
+                  { format: 'float32x3', shaderLocation: 0, offset: 0 },
+                ],
+              },
+            ],
           },
           layout: 'auto',
           fragment: {
@@ -37,5 +96,30 @@ export const usePipeline = (device?: GPUDevice | null) => {
         .then((pipeline) => setPipeline(pipeline));
     }
   }, [device]);
-  return pipeline;
+  return { pipeline, vertexBuffer, colorBuffer };
+};
+
+/**
+ *  group使用
+ * @param config
+ * @returns
+ */
+export const useGroup = (config?: {
+  groupDataEntries: GPUBindGroupEntry[] | null | undefined;
+  device: GPUDevice | null;
+  pipeline: GPURenderPipeline | null;
+}) => {
+  const group = useMemo(() => {
+    if (config) {
+      const { groupDataEntries, device, pipeline } = config;
+      if (device && pipeline && groupDataEntries) {
+        return device.createBindGroup({
+          entries: groupDataEntries,
+          layout: pipeline.getBindGroupLayout(0),
+        });
+      }
+    }
+    return null;
+  }, [config]);
+  return group;
 };
